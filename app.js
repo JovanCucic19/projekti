17678ab2e0c94e5d2e37784aba45dd2a5e7c7991
JovanCucic19 <jovan.cucic.19@gmail.com>
2016-05-03 17:36:53 +0200

    prvi commit

---
+++ app.js
@@ -0,0 +1,30 @@
+/**
+ * Created by jovan on 3.5.16..
+ */
+
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+   host : 'localhost',
+   user : 'root',
+   password : '',
+   database : 'drustvena'
+});
+
+
+con.query('SELECT * FROM korisnici',function(err,rows){
+    if(err) throw err;
+
+    console.log('Data received from Db:\n');
+    console.log(rows);
+});
+
+con.end(function (err) {
+   if(err != undefined){
+       console.log(err);
+   }
+});
+
+
+
+
